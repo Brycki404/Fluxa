f9109fa4e7b8e099d76b9740343c8632b2b8c642
Brycki404 <dustinbk@icloud.com>
2026-04-13 07:39:20 -0400

    Push!!!

---
+++ src/init.lua
@@ -0,0 +1,17 @@
+local Fluxa = {}
+
+Fluxa.AnimationAsset = require(script.AnimationAsset)
+Fluxa.AnimationPlayer = require(script.AnimationPlayer)
+Fluxa.BlendTree = require(script.BlendTree)
+Fluxa.Pose = require(script.Pose)
+Fluxa.Retargeting = require(script.Retargeting)
+Fluxa.UniversalJointWriter = require(script.UniversalJointWriter)
+
+Fluxa.IK = {}
+Fluxa.IK.CCD = require(script.IK.CCD)
+Fluxa.IK.FABRIK = require(script.IK.FABRIK)
+Fluxa.IK.FootPlanting = require(script.IK.FootPlanting)
+Fluxa.IK.LookAt = require(script.IK.LookAt)
+Fluxa.IK.TwoBoneIK = require(script.IK.TwoBoneIK)
+
+return Fluxa
